e93655f377c392ac62ddb5b534f49be1481c0399
merencan <mustafaerencan1@gmail.com>
2026-06-09 15:10:54 +0300

    Delete DessertRecipe.java

--- DessertRecipe.java
+++
@@ -1,6 +0,0 @@
-public class DessertRecipe extends Recipe {
-    public DessertRecipe(String name, int prepMinutes, int cookMinutes, int servings) {
-        super(name, prepMinutes, cookMinutes, servings);}
-    @Override
-    public String getCategory() {return "Dessert";}
-}
